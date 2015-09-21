85acb834ae24221c63e6e31bef6e078d3b746be6
Mark Janssen <mark@praseodym.net>
2015-09-21 10:43:29 +0200

    Initial commit

---
+++ src/main/java/ch/wisv/eventreg/EventregApplication.java
@@ -0,0 +1,12 @@
+package ch.wisv.eventreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventregApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventregApplication.class, args);
+    }
+}
