868430ae15aad7d61894c2cf9271a40f3da5b2a0
Rick Wieman <git@rickw.nl>
2015-10-07 00:48:11 +0200

    Adds primitive events controller
    * Controller currently supports:
      - Viewing all events
      - Viewing a creation form
      - Storing a created event
    * Adds first three events to landing page
    * Fixes jsDelivr URLs

--- src/main/java/ch/wisv/events/EventsApplication.java
+++ src/main/java/ch/wisv/events/EventsApplication.java
@@ -2,7 +2,11 @@ package ch.wisv.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+// Jsr310JpaConverters.class is necessary for correctly persisting e.g. LocalDateTime objects
+@EntityScan(basePackageClasses = {EventsApplication.class, Jsr310JpaConverters.class})
 @SpringBootApplication
 public class EventsApplication {
 
