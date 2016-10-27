edb634e684ad6c964c4e2f0101038141f9083a18
Sven Popping <sven@popping.co>
2016-10-27 17:48:00 +0200

    Add JavaDoc to EventStatus

--- src/main/java/ch/wisv/events/data/model/event/EventStatus.java
+++ src/main/java/ch/wisv/events/data/model/event/EventStatus.java
@@ -3,31 +3,58 @@ package ch.wisv.events.data.model.event;
 import static java.util.Arrays.stream;
 
 /**
- * Created by sven on 18/10/2016.
+ * Enum EventStatus
  */
 public enum EventStatus {
 
+    /**
+     * Possible event status
+     */
     PUBLISHED(1, "Published"),
     CONCEPT(2, "Concept"),
     NOT_PUBLISHED(3, "Not published");
 
+    /**
+     * ID of the status
+     */
     private final int id;
 
+    /**
+     * Display name of the status (for view purpose)
+     */
     private final String displayName;
 
+    /**
+     * Default constructor
+     * @param id ID of the status
+     * @param displayName Display name of the status
+     */
     EventStatus(int id, String displayName) {
         this.id = id;
         this.displayName = displayName;
     }
 
+    /**
+     * Get status by status ID
+     * @param index Status ID
+     * @return EventStatus corresponding to the ID
+     */
     public static EventStatus getStatus(int index) {
         return stream(EventStatus.values()).filter(status -> status.getId() == index).findFirst().orElse(null);
     }
 
+    /**
+     * Get display name of the EventStatus
+     * @return display name
+     */
     public String getDisplayName() {
         return displayName;
     }
 
+    /**
+     * Get id of the EventStatus
+     * @return id
+     */
     public int getId() {
         return id;
     }
