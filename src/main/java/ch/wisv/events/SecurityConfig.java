c8b214b7b3987a870f5ba19c43a4503b4dc3f565
Sven Popping <sven@popping.co>
2016-10-30 18:59:32 +0100

    Add JavaDoc to EventApplication and SecurityConfig

--- src/main/java/ch/wisv/events/SecurityConfig.java
+++ src/main/java/ch/wisv/events/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Should be replaced with CH Connect
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
