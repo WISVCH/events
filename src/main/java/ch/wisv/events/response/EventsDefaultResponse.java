351a7d6268ab6106b9228e0bca4e1c0a7078ca15
Sven Popping <sven@popping.co>
2016-10-30 18:33:35 +0100

    Add JavaDoc to responses

--- src/main/java/ch/wisv/events/response/EventsDefaultResponse.java
+++ src/main/java/ch/wisv/events/response/EventsDefaultResponse.java
@@ -8,36 +8,57 @@ import javax.persistence.Lob;
 import java.time.LocalDateTime;
 
 /**
- * Created by sven on 20/10/2016.
+ * EventsDefaultResponse.
  */
 @AllArgsConstructor
 public class EventsDefaultResponse {
 
+    /**
+     * Key of an Event.
+     */
     @Getter
     @Setter
     private String key;
 
+    /**
+     * Title of an Event
+     */
     @Getter
     @Setter
     private String title;
 
+    /**
+     * Description of an Event
+     */
     @Lob
     @Getter
     @Setter
     private String description;
 
+    /**
+     * Location of an Event
+     */
     @Getter
     @Setter
     private String location;
 
+    /**
+     * ImageURL of an Event
+     */
     @Getter
     @Setter
     private String imageURL;
 
+    /**
+     * Start of an Event
+     */
     @Getter
     @Setter
     private LocalDateTime startEvent;
 
+    /**
+     * End of an Event
+     */
     @Getter
     @Setter
     private LocalDateTime endEvent;
