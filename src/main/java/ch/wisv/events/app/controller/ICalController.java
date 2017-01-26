7a919767356b53c1238853837e358ad7c1065a27
Jan-Willem Manenschijn <janwillemm@wlan-145-94-168-170.wlan.tudelft.nl>
2017-01-26 21:28:02 +0100

    Generates an iCal overview of all events

--- src/main/java/ch/wisv/events/app/controller/ICalController.java
+++ src/main/java/ch/wisv/events/app/controller/ICalController.java
@@ -1,10 +1,20 @@
 package ch.wisv.events.app.controller;
 
+import biweekly.ICalVersion;
+import biweekly.ICalendar;
+import biweekly.io.text.ICalWriter;
+import ch.wisv.events.core.factory.ICalendarFactory;
 import ch.wisv.events.core.service.event.EventService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by janwillemm on 26/01/2017.
@@ -24,9 +34,28 @@ public class ICalController {
      * Get request on "/events/iCal" will show ical.
      *
      */
-    @ResponseBody
     @RequestMapping(value = "/iCal", method = RequestMethod.GET)
-    public String getAllEvents() {
-        return "";
+    public void getAllEvents(HttpServletResponse response) {
+        // Getting the iCal with the current available events
+        ICalendar ical = ICalendarFactory.createEventList(eventService.getAvailableEvents());
+
+        try {
+            // Create a temporary file to for the iCalWriter to write to.
+            File file = File.createTempFile("ical", ".ics");
+            ICalWriter writer = new ICalWriter(file, ICalVersion.V2_0);
+            writer.write(ical);
+            writer.close();
+
+            // Make the outputstream the same as our temporary file
+            IOUtils.copy(new FileInputStream(file), response.getOutputStream());
+
+            // And flush the file
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new RuntimeException("IOError writing file to output stream");
+        }
     }
+
+
+
 }
