465950695d2582ed2773635b1cd670fe71619ecd
Sven Popping <sven@popping.co>
2017-07-30 11:18:18 +0200

    Fix error in test with flyway

--- src/test/java/ch/wisv/events/EventsApplicationTest.java
+++ src/test/java/ch/wisv/events/EventsApplicationTest.java
@@ -3,6 +3,7 @@ package ch.wisv.events;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
@@ -26,8 +27,8 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @ComponentScan
-@EnableAutoConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude=FlywayAutoConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "flyway.enabled=false")
 @ActiveProfiles("test")
 public class EventsApplicationTest {
 
