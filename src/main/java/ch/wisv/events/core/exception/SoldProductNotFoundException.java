f3c6630587ea227ca3b88d88a3ef4d1fcb216cbc
Sven Popping <sven@popping.co>
2017-09-16 00:30:16 +0200

    Change exception to runtime

--- src/main/java/ch/wisv/events/core/exception/SoldProductNotFoundException.java
+++ src/main/java/ch/wisv/events/core/exception/SoldProductNotFoundException.java
@@ -16,7 +16,7 @@ package ch.wisv.events.core.exception;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-public class SoldProductNotFoundException extends Exception {
+public class SoldProductNotFoundException extends RuntimeException {
 
     /**
      * Constructor SoldProductNotFoundException creates a new SoldProductNotFoundException instance.
