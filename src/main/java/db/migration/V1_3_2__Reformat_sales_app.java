90284ac565128c52e2f2e2015c309e40e6313b52
Sven Popping <sven@popping.co>
2017-09-16 23:45:16 +0200

    Add unique to sold_product

---
+++ src/main/java/db/migration/V1_3_2__Reformat_sales_app.java
@@ -0,0 +1,35 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Copyright (c) 2016  W.I.S.V. 'Christiaan Huygens'
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class V1_3_2__Reformat_sales_app implements SpringJdbcMigration {
+
+    /**
+     * Executes this migration. The execution will automatically take place within a transaction, when the underlying
+     * database supports it.
+     *
+     * @param jdbcTemplate The jdbcTemplate to use to execute statements.
+     * @throws Exception when the migration failed.
+     */
+    @Override
+    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
+        jdbcTemplate.execute("ALTER TABLE public.unique_code ADD COLUMN unique_code VARCHAR(255)");
+    }
+}
