aa625e7ad2291887d36f623460ec07273dc20414
Sven Popping <sven@popping.co>
2017-09-16 21:24:55 +0200

    Remove default test

--- src/test/java/ch/wisv/events/EventsApplicationTest.java
+++ src/test/java/ch/wisv/events/EventsApplicationTest.java
@@ -1,39 +1,39 @@
-package ch.wisv.events;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Copyright (c) 2016  W.I.S.V. 'Christiaan Huygens'
- * <p>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p>
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-@RunWith(SpringRunner.class)
-@ComponentScan
-@EnableAutoConfiguration(exclude={FlywayAutoConfiguration.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "flyway.enabled=false")
-@ActiveProfiles("test")
-public class EventsApplicationTest {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//package ch.wisv.events;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * Copyright (c) 2016  W.I.S.V. 'Christiaan Huygens'
+// * <p>
+// * This program is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
+// * <p>
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU General Public License for more details.
+// * <p>
+// * You should have received a copy of the GNU General Public License
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// */
+//@RunWith(SpringRunner.class)
+//@ComponentScan
+//@EnableAutoConfiguration(exclude={FlywayAutoConfiguration.class})
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "flyway.enabled=false")
+//@ActiveProfiles("test")
+//public class EventsApplicationTest {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
