a0ef523a1756ddc173b6bfa18aa08e214481a2ae
Sven Popping <sven@popping.co>
2018-08-02 11:54:48 +0200

    Add comments to Study enum

--- src/main/java/ch/wisv/events/core/model/registration/Study.java
+++ src/main/java/ch/wisv/events/core/model/registration/Study.java
@@ -7,18 +7,39 @@ import lombok.Getter;
  */
 public enum Study {
 
+    /**
+     * Bachelor Applied Mathematics.
+     */
     BS_AM("Bachelor Applied Mathematics"),
 
+    /**
+     * Bachelor Computer Science and Engineering.
+     */
     BS_CSE("Bachelor Computer Science and Engineering"),
 
+    /**
+     * Double Bachelor (AP /AM).
+     */
     BS_DOUBLE("Double Bachelor (AP /AM)"),
 
+    /**
+     * Master Applied Mathematics.
+     */
     MS_AM("Master Applied Mathematics"),
 
+    /**
+     * Master Computer Engineering.
+     */
     MS_CE("Master Computer Engineering"),
 
+    /**
+     * Master Computer Science.
+     */
     MS_CS("Master Computer Science"),
 
+    /**
+     * Master Embedded Systems.
+     */
     MS_ES("Master Embedded Systems");
 
     /**
