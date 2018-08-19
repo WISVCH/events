a499f025a46aa20366791ad506d5b7dd5326a862
Sven Popping <sven@popping.co>
2018-08-19 16:58:42 +0200

    Fix checkstyle issues

--- src/main/java/ch/wisv/events/core/model/order/PaymentMethod.java
+++ src/main/java/ch/wisv/events/core/model/order/PaymentMethod.java
@@ -5,9 +5,29 @@ package ch.wisv.events.core.model.order;
  */
 public enum PaymentMethod {
 
-    CASH,        // User paid his order with cash
-    CARD,        // User paid his order by card
-    IDEAL,       // User paid his order via Mollie iDeal
-    CREDIT_CARD, // User paid his order via Mollie Credit Card
-    OTHER        // User paid his order via another method
+    /**
+     * User paid his order with cash.
+     */
+    CASH,
+
+    /**
+     * User paid his order by card.
+     */
+    CARD,
+
+    /**
+     * User paid his order via Mollie iDeal.
+     */
+    IDEAL,
+
+    /**
+     * User paid his order via Mollie Credit Card.
+     */
+    CREDIT_CARD,
+
+    /**
+     * User paid his order via another method.
+     */
+    OTHER
+
 }
