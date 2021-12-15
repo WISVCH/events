7b4ece20ca562ae55609f51617c96c13a90d0ee5
Bram van Kooten <bram.vankooten@hotmail.com>
2021-12-15 16:05:31 +0100

    Cleanup ScanDto

--- src/main/java/ch/wisv/events/sales/model/ScanDto.java
+++ src/main/java/ch/wisv/events/sales/model/ScanDto.java
@@ -1,17 +1,12 @@
 package ch.wisv.events.sales.model;
 
-import ch.wisv.events.core.model.ticket.TicketStatus;
 import lombok.Data;
-
-import javax.validation.constraints.NotNull;
 
 @Data
 public class ScanDto {
 
-    @NotNull
     private String productTitle;
 
-    @NotNull
     private String ownerName;
 
     public ScanDto(String productTitle, String ownerName) {
