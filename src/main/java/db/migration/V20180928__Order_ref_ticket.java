6f2e4304bea21e06a2edf0088baf0a3c1e6faf33
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2022-04-03 14:00:05 +0200

    upgrade flyway (#385)
    * upgrade flyway
    
    * update migrations to use BaseJavaMigration
    
    Co-authored-by: Julian <jullian_1999@outlook.com>

--- src/main/java/db/migration/V20180928__Order_ref_ticket.java
+++ src/main/java/db/migration/V20180928__Order_ref_ticket.java
@@ -1,24 +1,28 @@
 package db.migration;
 
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+import java.sql.Statement;
 
 /**
  * DB migration.
  */
-public class V20180928__Order_ref_ticket implements SpringJdbcMigration {
+public class V20180928__Order_ref_ticket extends BaseJavaMigration {
 
     /**
      * Executes this migration. The execution will automatically take place within a transaction, when the underlying
      * database supports it.
      *
-     * @param jdbcTemplate The jdbcTemplate to use to execute statements.
+     * @param context of type Context
+     * @throws Exception when something is wrong
      */
-    @Override
-    public void migrate(JdbcTemplate jdbcTemplate) {
-        jdbcTemplate.execute("ALTER TABLE public.ticket ADD COLUMN order_id INTEGER");
-        jdbcTemplate.execute("ALTER TABLE ONLY public.ticket ADD CONSTRAINT fk6pbiwpljchpolob40s7di41y4 FOREIGN KEY (order_id) REFERENCES "
-                                     + "public.orders(id);");
+    public void migrate(Context context) throws Exception {
+        try (Statement select = context.getConnection().createStatement()) {
+            select.execute("ALTER TABLE public.ticket ADD COLUMN order_id INTEGER");
+            select.execute("ALTER TABLE ONLY public.ticket ADD CONSTRAINT fk6pbiwpljchpolob40s7di41y4 FOREIGN KEY (order_id) REFERENCES "
+                    + "public.orders(id);");
+        }
     }
 
 }
--- src/main/java/db/migration/V20190129__Add_reservable_to_product.java
+++ src/main/java/db/migration/V20190129__Add_reservable_to_product.java
@@ -1,28 +1,30 @@
 package db.migration;
 
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
 
+import java.sql.Statement;
 
 
 /**
  * DB migration which adds a reservable column to products, setting the already existing products to reservable = true
  */
-public class V20190129__Add_reservable_to_product implements SpringJdbcMigration {
+public class V20190129__Add_reservable_to_product extends BaseJavaMigration {
 
     /**
      * Executes this migration. The execution will automatically take place within a transaction, when the underlying
      * database supports it.
      *
-     * @param jdbcTemplate The jdbcTemplate to use to execute statements.
-     *
+     * @param context of type Context
+     * @throws Exception when something is wrong
      */
-    @Override
-    public void migrate(JdbcTemplate jdbcTemplate) {
-        // Remove column ch_username.
-        jdbcTemplate.execute("ALTER TABLE public.product ADD COLUMN reservable BOOLEAN");
-        jdbcTemplate.execute("UPDATE public.product SET reservable = TRUE");
-        jdbcTemplate.execute("ALTER TABLE public.product ALTER COLUMN reservable SET NOT NULL");
+    public void migrate(Context context) throws Exception {
+        try (Statement select = context.getConnection().createStatement()) {
+            // Remove column ch_username.
+            select.execute("ALTER TABLE public.product ADD COLUMN reservable BOOLEAN");
+            select.execute("UPDATE public.product SET reservable = TRUE");
+            select.execute("ALTER TABLE public.product ALTER COLUMN reservable SET NOT NULL");
+        }
     }
 
 }
