a14ef4a6690adccb07425595997d49d763b74cc9
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-07-06 16:52:12 +0200

    Cleanup TicketServiceImpl

--- src/main/java/ch/wisv/events/core/service/ticket/TicketServiceImpl.java
+++ src/main/java/ch/wisv/events/core/service/ticket/TicketServiceImpl.java
@@ -20,11 +20,7 @@ import java.util.UUID;
 import ch.wisv.events.core.service.event.EventService;
 import ch.wisv.events.core.service.mail.MailService;
 import ch.wisv.events.core.util.QrCode;
-import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.stereotype.Service;
 
 /**
@@ -122,7 +118,7 @@ public class TicketServiceImpl implements TicketService {
     }
 
     /**
-     * Get all Tickets by a Order.
+     * Get all Tickets of an Order.
      *
      * @param order of type Order
      *
