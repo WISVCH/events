75904659e1cb38c8e40a916fe7c97d929ad24b79
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-22 15:23:57 +0200

    Add second decimal test for mollie payment request

--- src/test/java/ch/wisv/events/webshop/service/PaymentsServiceImplTest.java
+++ src/test/java/ch/wisv/events/webshop/service/PaymentsServiceImplTest.java
@@ -7,11 +7,7 @@ import be.woutschoovaerts.mollie.data.payment.PaymentRequest;
 import be.woutschoovaerts.mollie.data.payment.PaymentResponse;
 import be.woutschoovaerts.mollie.handler.PaymentHandler;
 import ch.wisv.events.ServiceTest;
-import ch.wisv.events.core.exception.normal.PaymentsStatusUnknown;
-import ch.wisv.events.core.exception.runtime.PaymentsConnectionException;
-import ch.wisv.events.core.exception.runtime.PaymentsInvalidException;
 import ch.wisv.events.core.model.customer.Customer;
-import ch.wisv.events.core.model.event.Event;
 import ch.wisv.events.core.model.order.Order;
 import ch.wisv.events.core.model.order.OrderProduct;
 import ch.wisv.events.core.model.order.OrderStatus;
@@ -19,38 +15,24 @@ import ch.wisv.events.core.model.order.PaymentMethod;
 import ch.wisv.events.core.model.product.Product;
 import ch.wisv.events.core.service.mail.MailService;
 import ch.wisv.events.core.service.order.OrderService;
-import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHttpResponse;
-import org.apache.http.message.BasicStatusLine;
-import org.json.simple.JSONObject;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Matchers.any;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.security.core.parameters.P;
 
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 /**
  * PaymentsServiceImpl test.
@@ -97,6 +79,42 @@ public class PaymentsServiceImplTest extends ServiceTest {
         when(handler.createPayment(Mockito.any())).thenReturn(paymentResponse);
 
         assertEquals(link.getHref(), paymentsService.getMollieUrl(order));
+    }
+
+    /**
+     * Test get payments mollie url with decimals.
+     *
+     * @throws Exception when something goes wrong
+     */
+    @Test
+    public void testCreateMolliePaymentRequestFromOrder() throws Exception {
+        List<Product> products = new ArrayList<>();
+
+        // Use a product ending on .05 to ensure correct rounding of decimals
+        Product product = new Product();
+        product.setTitle("Product product1");
+        product.setCost(13.95d);
+        product.setSellStart(LocalDateTime.now());
+        product.setProducts(new ArrayList<>());
+        product.setMaxSoldPerCustomer(1);
+
+        Order order = this.createOrder(createCustomer(), products, OrderStatus.PENDING,"WISVCH.1234");
+
+        // Add two
+        OrderProduct orderProduct = new OrderProduct(product, product.getCost(), 3L);
+        order.addOrderProduct(orderProduct);
+
+        PaymentRequest paymentRequest = paymentsService.createMolliePaymentRequestFromOrder(order);
+
+
+        assertEquals(paymentRequest.getConsumerName(), Optional.of("Test user"));
+
+        /**
+         * Assert that price rounding works correctly. Second decimal is mandatory for Mollie.
+         * 13.95+2*13.95+0.35 = 42.20
+         * Ensure that 42.20 is returned and not 42.2
+         */
+        assertEquals(paymentRequest.getAmount().getValue().toString(), "42.20");
     }
 
 
