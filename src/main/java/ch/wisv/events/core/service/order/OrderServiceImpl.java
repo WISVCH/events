00266905e447639a9cb8752322dd629e2a62711e
Dany Sluijk <me@dany.dev>
2022-12-23 14:27:58 +0100

    fix(order): fix import

--- src/main/java/ch/wisv/events/core/service/order/OrderServiceImpl.java
+++ src/main/java/ch/wisv/events/core/service/order/OrderServiceImpl.java
@@ -38,7 +38,7 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 /**
