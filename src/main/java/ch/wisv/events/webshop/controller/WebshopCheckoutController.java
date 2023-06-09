5e8b2278d167d065be04367c6232cde01574bdcc
KasperVaessen <kasper.vaessen@hotmail.com>
2023-06-09 18:30:21 +0200

    Added feature to make sure users have to agree to the General terms and conditions

--- src/main/java/ch/wisv/events/webshop/controller/WebshopCheckoutController.java
+++ src/main/java/ch/wisv/events/webshop/controller/WebshopCheckoutController.java
@@ -28,6 +28,8 @@ public class WebshopCheckoutController extends WebshopController {
     /** Error message no products in Order. */
     private static final String ERROR_MESSAGE_ORDER_WITHOUT_PRODUCTS = "Shopping basket can not be empty!";
 
+    private static final String ERROR_MESSAGE_ORDER_NOT_AGREED = "You have to agree with the General Terms and Conditions to proceed to checkout.";
+
     /** Username of the user that created this order. */
     private static final String USERNAME_ORDER_CREATED = "events-webshop";
 
@@ -64,6 +66,11 @@ public class WebshopCheckoutController extends WebshopController {
     @PostMapping
     public String checkoutShoppingBasket(RedirectAttributes redirect, @ModelAttribute OrderProductDto orderProductDto) {
         try {
+            if (orderProductDto.getAgreedGTC() == Boolean.FALSE) {
+                redirect.addFlashAttribute(MODEL_ATTR_ERROR, ERROR_MESSAGE_ORDER_NOT_AGREED);
+
+                return REDIRECT_EVENTS_HOME;
+            }
             if (orderProductDto.getProducts().isEmpty()) {
                 redirect.addFlashAttribute(MODEL_ATTR_ERROR, ERROR_MESSAGE_ORDER_WITHOUT_PRODUCTS);
 
