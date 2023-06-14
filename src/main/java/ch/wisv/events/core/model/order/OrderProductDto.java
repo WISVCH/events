5a262b0aa41cfdbf39cbb38c1b3ab17906d06eee
KasperVaessen <58064813+KasperVaessen@users.noreply.github.com>
2023-06-14 12:19:02 +0200

    Added feature to make sure users have to agree to the General terms a… (#432)
    * Added feature to make sure users have to agree to the General terms and conditions
    
    * Edited link to GTC.
    
    * removed unnecessary margins from css

--- src/main/java/ch/wisv/events/core/model/order/OrderProductDto.java
+++ src/main/java/ch/wisv/events/core/model/order/OrderProductDto.java
@@ -10,11 +10,13 @@ public class OrderProductDto {
      * Field product should contain Product keys and number of webshop of that product.
      */
     HashMap<String, Long> products;
+    Boolean agreedGTC;
 
     /**
      * Constructor.
      */
     public OrderProductDto() {
         this.products = new HashMap<>();
+        this.agreedGTC = Boolean.FALSE;
     }
 }
