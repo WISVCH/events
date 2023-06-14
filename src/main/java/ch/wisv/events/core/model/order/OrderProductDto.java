d5b89d52fd489be25f4f314495bf2e806584739e
Julian van Dijk <superjuul99@gmail.com>
2023-06-14 12:22:17 +0200

    Revert "Added feature to make sure users have to agree to the General terms a… (#432)"
    This reverts commit 5a262b0aa41cfdbf39cbb38c1b3ab17906d06eee.

--- src/main/java/ch/wisv/events/core/model/order/OrderProductDto.java
+++ src/main/java/ch/wisv/events/core/model/order/OrderProductDto.java
@@ -10,13 +10,11 @@ public class OrderProductDto {
      * Field product should contain Product keys and number of webshop of that product.
      */
     HashMap<String, Long> products;
-    Boolean agreedGTC;
 
     /**
      * Constructor.
      */
     public OrderProductDto() {
         this.products = new HashMap<>();
-        this.agreedGTC = Boolean.FALSE;
     }
 }
