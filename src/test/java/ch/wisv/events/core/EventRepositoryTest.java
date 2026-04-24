0318f36ddf302924e7102f5c8459ec83c9b30796
Robert van Dijk <robert@citroenvlinder17.nl>
2026-04-24 16:10:43 +0200

    Fix tests

--- src/test/java/ch/wisv/events/core/EventRepositoryTest.java
+++ src/test/java/ch/wisv/events/core/EventRepositoryTest.java
@@ -8,6 +8,7 @@ import ch.wisv.events.utils.LdapGroup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -23,6 +24,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @TestPropertySource(properties = "spring.flyway.enabled=false")
 public class EventRepositoryTest {
